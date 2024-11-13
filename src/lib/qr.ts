9d997a4c6103cd26eca6b97ca94443196bc984a9
Mohamed Diomande <gdiomande7907@gmail.com>
2024-11-12 22:13:11 -0500

    added qr

--- src/lib/qr.ts
+++ src/lib/qr.ts
@@ -2,8 +2,15 @@ import QRCode from 'qrcode'
 
 export async function generateQRCode(shortCode: string): Promise<string> {
   try {
+    if (!process.env.NEXT_PUBLIC_APP_URL) {
+      throw new Error('NEXT_PUBLIC_APP_URL environment variable is not set')
+    }
+
     // Generate URL for the QR code
-    const url = `${process.env.NEXT_PUBLIC_APP_URL}/r/${shortCode}`
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL.replace(/\/$/, '') // Remove trailing slash if present
+    const url = `${baseUrl}/r/${shortCode}`
+    
+    console.log('Generating QR code for URL:', url) // Debug log
     
     // Generate QR code as data URL
     const qrDataUrl = await QRCode.toDataURL(url, {
